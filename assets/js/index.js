d97c3e5b3e70ccf9b0afcd64c744bfae92a824f0
ZikaZaki <zika.github@gmail.com>
2022-07-28 00:33:18 +0300

    Fixed linter errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,48 @@
-function toggleMenu(menu){
-    menu.classList.toggle('open');
-    if(menu.classList.contains('open')){
-        myBlurFunction(1);
-    }else{
-        myBlurFunction(0);
-    } 
-}
+const myBlurFunction = function (state) {
+  /* state can be 1 or 0 */
+  const blurElements = [document.getElementById('main'), document.getElementById('logo')];
 
-var myBlurFunction = function(state) {
-    /* state can be 1 or 0 */
-    var blurElements = [document.getElementById('main'),document.getElementById('logo')];
+  if (state) {
+    blurElements.forEach((e) => {
+      e.classList.add('blur');
+    });
 
-    if (state) {
-        blurElements.forEach(e => {
-            e.classList.add('blur');
-        });
+    // Setting the background-color of the close icon to white
+    document.getElementById('bar1').style.backgroundColor = '#fff';
+    document.getElementById('bar3').style.backgroundColor = '#fff';
+  } else {
+    blurElements.forEach((e) => {
+      e.classList.remove('blur');
+    });
 
-        //Setting the background-color of the close icon to white    
-        document.getElementById('bar1').style.backgroundColor='#fff';
-        document.getElementById('bar3').style.backgroundColor='#fff';
-    } else {
-        blurElements.forEach(e => {
-            e.classList.remove('blur');
-        });
-
-        //Resetting the background-color of the close icon to its original color
-        document.getElementById('bar1').style.backgroundColor='#3a4adb';
-        document.getElementById('bar3').style.backgroundColor='#3a4adb';
-    }
+    // Resetting the background-color of the close icon to its original color
+    document.getElementById('bar1').style.backgroundColor = '#3a4adb';
+    document.getElementById('bar3').style.backgroundColor = '#3a4adb';
+  }
 };
+
+const toggleMenu = function (menu) {
+  menu.classList.toggle('open');
+  if (menu.classList.contains('open')) {
+    myBlurFunction(1);
+  } else {
+    myBlurFunction(0);
+  }
+};
+
+toggleMenu.call();
+
 // const close_icon = document.getElementById('hamburger-icon');
 // close_icon.onclick = () => {
 //     close_icon.style.backgroundColor = '#fff';
 //     this.style.backgroundColor = '#fff';
-
 // };
-
 // function dynamic_menue (){
 //     const div = document.createElement('div');
 //     div.className = 'dynamic-menue';
 //     const ul = document.createElement('ul');
 //     ul.className = 'ul';
-    
 //     const tab_links = ['Portfolio', 'About', 'Contact'];
-    
 //     const li = document.createElement('li');
 //     li.className = 'li';
 //     const close_icon = document.createElement('img');
@@ -60,14 +59,12 @@ var myBlurFunction = function(state) {
 //         li.appendChild(a);
 //         ul.appendChild(li);
 //     }
-
 //     div.appendChild(ul);
 //     document.body.appendChild(div);
 //     const cssLink = document.createElement('link');
 //     cssLink.rel = 'stylesheet';
 //     cssLink.href = '/css/dynamic-menue.css';
 //     document.head.appendChild(cssLink);
-
 //     div.onclick = function(e){
 //         if(e.target.className === 'close-icon'){
 //             div.remove();
@@ -75,5 +72,4 @@ var myBlurFunction = function(state) {
 //         };
 //     }
 // }
-
 // document.getElementById('humberger-icon').onclick = dynamic_menue;
