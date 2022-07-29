81eaeafccba2d197ed3249db78aac7b0642d2146
ZikaZaki <zika.github@gmail.com>
2022-07-29 18:43:03 +0300

    Added validation using Regx to JavaScript

--- assets/js/contact-validation.js
+++ assets/js/contact-validation.js
@@ -1,10 +1,31 @@
-document.querySelector('.submit').addEventListener('click', (event)={
-   const mail = document.getElementById('email').value;
-   const errMsg = docuemnt.querySelector('.error');
-   const toLowerCase = (str) => str === str.toLowerCase();
-
-   if (!toLowerCase(email)) {
+const validRegex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+const email = document.forms.contactForm.userEmail;
+const emailErrMsg = document.getElementById('emailErrMsg');
+const contactSubmitBtn = document.forms.contactForm.submitBtn;
+contactSubmitBtn.addEventListener('click', (event) => {
+  if (!email.value.match(validRegex)) {
     event.preventDefault();
-    errMsg.textContent = 'Please use only lower-case for email-address';
-   }
+    emailErrMsg.textContent = 'Please enter email-address in lowercase!.';
+  }
 });
+
+// const form = document.getElementById('contactForm');
+// const errMsg = document.getElementById('errorMsg');
+// const email = document.getElementById('email').value;
+
+// form.addEventListener('click', (event) => {
+//    if (!email === email.toLowerCase()) {
+//       event.preventDefault();
+//       errMsg.textContent = 'Please enter email-address in lowercase!.';
+//    }
+// });
+
+// document.getElementById('contactSubmitBtn').addEventListener('click', (event) => {
+//    const email = document.getElementById('email').value;
+//    const errorMsg = document.getElementById('errorMsg');
+
+//    if (!email === email.toLowerCase()) {
+//       event.preventDefault();
+//       errorMsg.textContent = 'Please enter email-address in lowercase.';
+//    }
+// });
