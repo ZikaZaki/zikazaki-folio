9574fdf818f679f9f90cc79bb9bbbf34019ef53a
Restyled.io <commits@restyled.io>
2022-07-29 19:18:31 +0000

    Restyled by prettier

--- assets/js/local-data.js
+++ assets/js/local-data.js
@@ -1,26 +1,26 @@
 /*= ===============Declaration of The Input Fields to be Stored in
  * JSON================ */
-const userName = document.getElementById('name');
-const userEmail = document.getElementById('email');
-const userMsg = document.getElementById('msg');
+const userName = document.getElementById("name");
+const userEmail = document.getElementById("email");
+const userMsg = document.getElementById("msg");
 
 /*= ============This Function Stores The Form Data into JSON====================
  */
 function storeData() {
   const jsonData = {
-    name : userName.value,
-    email : userEmail.value,
-    message : userMsg.value,
+    name: userName.value,
+    email: userEmail.value,
+    message: userMsg.value,
   };
-  localStorage.setItem('contactForm', JSON.stringify(jsonData));
+  localStorage.setItem("contactForm", JSON.stringify(jsonData));
 }
 
 /*= ============This Function Parse Data Stored in JSON to
  * Form==================== */
-document.addEventListener('DOMContentLoaded', () => {
-  const storedData = JSON.parse(localStorage.getItem('contactForm'));
+document.addEventListener("DOMContentLoaded", () => {
+  const storedData = JSON.parse(localStorage.getItem("contactForm"));
   if (storedData) {
-    userName.value = storedData.name;   // JSON.parse(storedData).name;
+    userName.value = storedData.name; // JSON.parse(storedData).name;
     userEmail.value = storedData.email; // JSON.parse(storedData).email;
     userMsg.value = storedData.message; // JSON.parse(storedData).message;
   }
