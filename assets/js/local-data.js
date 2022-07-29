33f3a5ae54e78ebab3dff818af4163f5cbccd292
GraceM <mara234216@gmail.com>
2022-07-29 20:07:58 +0200

    Added storDate function to local-data.js file

--- assets/js/local-data.js
+++ assets/js/local-data.js
@@ -1,3 +1,12 @@
 const userName = document.getElementById('name');
 const userEmail = document.getElementById('email');
 const userMsg =document.getElementById('msg');
+
+function storeData(){
+    localData = {
+        name = userName.value,
+        email = userEmail.value,
+        message = userMsg.value,
+    };
+    localStorage.setItem('form', JSON.stringify(localData));
+}
