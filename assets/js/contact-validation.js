9f0ff1696d7636ab68f4dd6c06645c25f963360e
Restyled.io <commits@restyled.io>
2022-07-29 15:55:15 +0000

    Restyled by prettier

--- assets/js/contact-validation.js
+++ assets/js/contact-validation.js
@@ -1,12 +1,12 @@
 const validRegex =
-    /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+  /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
 const email = document.forms.contactForm.userEmail;
-const emailErrMsg = document.getElementById('emailErrMsg');
+const emailErrMsg = document.getElementById("emailErrMsg");
 const contactSubmitBtn = document.forms.contactForm.submitBtn;
-contactSubmitBtn.addEventListener('click', (event) => {
+contactSubmitBtn.addEventListener("click", (event) => {
   if (!email.value.match(validRegex)) {
     event.preventDefault();
-    emailErrMsg.textContent = 'Please enter email-address in lowercase!.';
+    emailErrMsg.textContent = "Please enter email-address in lowercase!.";
   }
 });
 
