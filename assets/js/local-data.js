89586246126f15f2c45cde6c9fcb3239038ad82a
ZikaZaki <zika.github@gmail.com>
2022-07-29 22:17:50 +0300

    Fixed linters in local-data.js file

--- assets/js/local-data.js
+++ assets/js/local-data.js
@@ -1,29 +1,29 @@
-/*================Declaration of The Input Fields to be Stored in JSON================ */
+/*= ===============Declaration of The Input Fields to be Stored in JSON================ */
 const userName = document.getElementById('name');
 const userEmail = document.getElementById('email');
-const userMsg =document.getElementById('msg');
+const userMsg = document.getElementById('msg');
 
-/*=============This Function Stores The Form Data into JSON==================== */
-function storeData(){
-    jsonData = {
-        name: userName.value,
-        email: userEmail.value,
-        message: userMsg.value,
-    };
-    localStorage.setItem('contactForm', JSON.stringify(jsonData));
+/*= ============This Function Stores The Form Data into JSON==================== */
+function storeData() {
+  const jsonData = {
+    name: userName.value,
+    email: userEmail.value,
+    message: userMsg.value,
+  };
+  localStorage.setItem('contactForm', JSON.stringify(jsonData));
 }
 
-/*=============This Function Parse Data Stored in JSON to Form==================== */
+/*= ============This Function Parse Data Stored in JSON to Form==================== */
 document.addEventListener('DOMContentLoaded', () => {
-    const storedData = JSON.parse(localStorage.getItem('contactForm'));
-    if (storedData) {
-        userName.value = storedData.name; // JSON.parse(storedData).name;
-        userEmail.value = storedData.email;  // JSON.parse(storedData).email;
-        userMsg.value = storedData.message; // JSON.parse(storedData).message;
-    }
+  const storedData = JSON.parse(localStorage.getItem('contactForm'));
+  if (storedData) {
+    userName.value = storedData.name; // JSON.parse(storedData).name;
+    userEmail.value = storedData.email; // JSON.parse(storedData).email;
+    userMsg.value = storedData.message; // JSON.parse(storedData).message;
+  }
 });
 
-/*==============Call The Store Function for Every Input Field================== */
+/*= =============Call The Store Function for Every Input Field================== */
 userName.onchange = storeData;
 userEmail.onchange = storeData;
 userMsg.onchange = storeData;
